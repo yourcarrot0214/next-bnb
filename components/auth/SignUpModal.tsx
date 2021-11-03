f2df19afe138d9eb4918a62cb3a67903410f91d4
yourcarrot0214 <stylenbs@gmail.com>
2021-11-03 21:58:17 +0900

    10.3 common button component

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -9,6 +9,7 @@ import Input from "../common/Input";
 import { monthList, dayList, yearList } from "../../lib/staticData";
 import palette from "../../styles/palette";
 import Selector from "../common/Selector";
+import Button from "../common/Button";
 
 const Container = styled.div`
   width: 568px;
@@ -61,6 +62,12 @@ const Container = styled.div`
     .sign-up-modal-birthday-year-selector {
       width: 33.3333%;
     }
+  }
+
+  .sign-up-modal-submit-button-wrapper {
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${palette.gray_eb};
   }
 `;
 
@@ -179,6 +186,9 @@ const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
             onChange={onChangeBirthYear}
           />
         </div>
+      </div>
+      <div className="sign-up-modal-submit-button-wrapper">
+        <Button type="submit">가입하기</Button>
       </div>
     </Container>
   );
